614dcebcf84a43e866f1edfa8fe0ff7c71b1d3ce
MinaAyman123 <aimanmina50@gmail.com>
2025-07-26 02:23:14 +0300

    New Tic Tax To

--- Tic_Tax_To.py
+++ Tic_Tax_To.py
@@ -1,94 +1,67 @@
+from random import choice
+
 def create_empty_board():
-    board = [['1', '2', '3'],
-             ['4', '5', '6'],
-             ['7', '8', '9']]
-    return board
+  return [
+      [1,2,3],
+      [4,5,6],
+      [7,8,9]
+        ]
 
 def show_board(board):
-    for row in board:
-        print('\t'.join(row))
+  for i in range(3):
+    for j in range(3):
+      print(board[i][j],end="      ")
     print()
 
 def set_players():
-    from random import choice
-    player1 = choice(['X', 'O'])
-    player2 = 'O' if player1 == 'X' else 'X'
-    return player1, player2
+  return choice(['X', 'O'])
 
 def take_input(board, player):
-    while True:
-        player_input = input('Please Enter a number between 1-9 representing an empty position: ')
-        if player_input == '1' and board[0][0].isdigit():
-            board[0][0] = player
-            break
-        elif player_input == '2' and board[0][1].isdigit():
-            board[0][1] = player
-            break
-        elif player_input == '3' and board[0][2].isdigit():
-            board[0][2] = player
-            break
-        elif player_input == '4' and board[1][0].isdigit():
-            board[1][0] = player
-            break
-        elif player_input == '5' and board[1][1].isdigit():
-            board[1][1] = player
-            break
-        elif player_input == '6' and board[1][2].isdigit():
-            board[1][2] = player
-            break
-        elif player_input == '7' and board[2][0].isdigit():
-            board[2][0] = player
-            break
-        elif player_input == '8' and board[2][1].isdigit():
-            board[2][1] = player
-            break
-        elif player_input == '9' and board[2][2].isdigit():
-            board[2][2] = player
-            break
-        else:
-            print('Invalid Choice. Try again.')
-    show_board(board)
+  while True:
+      n=int(input("Please Enter a number between 1,9 represents an empty position:"))
+      for i in range(3):
+        for j in range(3):
+            if n==board[i][j]:
+              board[i][j]=player
+              return
+      print("Invaliied input")
 
 def check_full_board(board):
-    for row in board:
-        for col in row:
-            if col.isdigit():
-                return False
-    return True
+  for i in range(3):
+    for j in range(3):
+      if isinstance(board[i][j],int):
+        return False
+  return True
 
 def check_win(board):
-    lines = [
-        board[0], board[1], board[2],
-        [board[0][0], board[1][0], board[2][0]],
-        [board[0][1], board[1][1], board[2][1]],
-        [board[0][2], board[1][2], board[2][2]],
-        [board[0][0], board[1][1], board[2][2]],
-        [board[0][2], board[1][1], board[2][0]]
-    ]
-    for line in lines:
-        if line[0] == line[1] == line[2] and not line[0].isdigit():
-            return True
-    return False
+    for row in board:
+        if row[0] == row[1] == row[2] and isinstance(row[0], str):
+            return f"{row[0]} wins"
+
+    for col in range(3):
+        if board[0][col] == board[1][col] == board[2][col] and isinstance(board[0][col], str):
+            return f"{board[0][col]} wins"
+
+    if board[0][0] == board[1][1] == board[2][2] and isinstance(board[0][0], str):
+        return f"{board[0][0]} wins"
+
+    if board[0][2] == board[1][1] == board[2][0] and isinstance(board[0][2], str):
+        return f"{board[0][2]} wins"
 
 def play():
-    player1, player2 = set_players()
-    print("Player1:", player1)
-    print("Player2:", player2, '\n')
-    board = create_empty_board()
+  board=create_empty_board()
+  player=set_players()
+  show_board(board)
+  while True:
+    take_input(board,player)
     show_board(board)
+    if check_win(board):
+      print(check_win(board))
+      break
+    if check_full_board(board):
+      print("ITs Tie")
+      break
+    player= "O" if player=="X" else "X"
 
-    while True:
-        for player in [player1, player2]:
-            print(f"{player}'s turn")
-            take_input(board, player)
-            if check_win(board):
-                print(f"{player} wins!")
-                show_board(board)
-                return
-            if check_full_board(board):
-                print("Game Finished. It's a tie!")
-                show_board(board)
-                return
-
-if __name__ == '__main__':
+if __name__=='__main__':
     play()
